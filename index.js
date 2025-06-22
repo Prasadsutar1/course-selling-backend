d08bef6dd38d603bafef26948af7a393816319e0
Prasadsutar1 <132896103+Prasadsutar1@users.noreply.github.com>
2025-06-22 17:17:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get("/",(req,res)=> {
-    res.send("hello this is the landing the page");
+    res.send("This is the Express server");
 });
 
 
